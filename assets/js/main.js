3cd73784ef861da9dfc4433f5fb65988afbfdebe
unknown <azizovagultakin@gmail.com>
2023-03-10 11:50:25 +0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,39 @@
+// var divQuery=document.querySelectorAll("el1")
+// var pelement=document.createElement("table")
+// var text=document.createTextNode("div")
+
+
+var div=document.getElementById("el1")
+var pElement=document.createElement("tabel")
+pElement.appendChild("tabel")
+
+pElement.appendChild("tr")
+const people=[
+
+
+{
+    Name:"Lale",
+    Surname:"Azizova",
+    Age:22
+},
+{
+    Name:"Nezri",
+    Surname:"Ismailova",
+    Age:19
+},
+{
+    Name:"Gulzar",
+    Surname:"Veliyeva",
+    Age:23
+},
+{
+    Name:"Sema",
+    Surname:"Huseynli",
+    Age:21
+}
+]
+for( let i=0;i<people.length;i++){
+    var pElement=document.createElement("tabel")
+    // pElement.innerText=()
+
+}
